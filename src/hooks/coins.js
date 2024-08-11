b1bd5fd4a893ea3dcdcc56a1ba64179116d5d594
Alejandro Valenzuela <tzalejo@gmail.com>
2024-08-11 15:02:45 -0300

    formateo

--- src/hooks/coins.js
+++ src/hooks/coins.js
@@ -3,9 +3,9 @@ import usdcImg from './../assets/coins/usdc.png';
 import usdtImg from './../assets/coins/usdt.png';
 
 const coins = {
-  dai: daiImg,
-  usdc: usdcImg,
-  usdt: usdtImg,
+    dai: daiImg,
+    usdc: usdcImg,
+    usdt: usdtImg,
 };
 
 export default coins;
